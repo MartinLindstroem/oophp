569f66c6284e32cc30e532e80d6c9efef9a03201
Martin Lindström <martinllindstroem@gmail.com>
2020-05-19 10:03:59 +0200

    Created views for the content-blogg

---
+++ view/content/blogpost.php
@@ -0,0 +1,13 @@
+<?php
+// if ($content->filter == "markdown") {
+//     $content->data = $filter->markdown($content->data);
+// }
+?>
+
+<article>
+    <header>
+        <h1><?= esc($content->title) ?></h1>
+        <p><i>Published: <time datetime="<?= esc($content->published_iso8601) ?>" pubdate><?= esc($content->published) ?></time></i></p>
+    </header>
+    <?= $content->data ?>
+</article>
